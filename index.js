16ee50183671cc801f9f4f66e9d853128df7e63f
unknown <francisrodrigues1996@gmail.com>
2021-08-31 23:22:00 -0300

    Aula 03 - Edpuzzle 01

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-const app = require('express')();
+const app = require("express")();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+let clients = [
+  { id: 1, nome: "Francis" },
+  { id: 2, nome: "Rogério" },
+  { id: 3, nome: "Lindomar" },
+];
+
+app.get("/clients", (req, res) => res.json(clients));
+
+app.get("/clients/:id", (req, res) =>
+  res.json(clients.filter((value) => value.id == req.params.id))
+);
+
+app.post("/clients", (req, res) => {
+  const client = req.body;
+  clients.push(client);
+  res.json(client);
+});
+
+app.put("/clients/:id", (req, res) => {
+  const id = req.params.id;
+  const nome = req.body.nome;
+
+  let client = clients.filter((value) => value.id == id);
+
+  client[0].nome = nome;
+
+  res.json(client[0]);
+});
+
+app.delete("/clients/:id", (req, res) =>
+  res.json(clients.filter((value) => value.id != req.params.id))
+);
+
+app.listen(3000);
